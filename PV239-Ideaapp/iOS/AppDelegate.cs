d3da058e9e6933e3d60de681b0788ebc144e2038
Michal Iríček <kecirimisko@gmail.com>
2018-06-01 22:40:49 +0200

    IOS authetication

--- PV239-Ideaapp/iOS/AppDelegate.cs
+++ PV239-Ideaapp/iOS/AppDelegate.cs
@@ -11,12 +11,47 @@ using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
 namespace PV239_IdeaApp.iOS
 {
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : FormsApplicationDelegate
+	public partial class AppDelegate : FormsApplicationDelegate, IAuthenticate
 	{
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+
+        public async Task<bool> Authenticate()
+        {
+            var success = false;
+            var message = string.Empty;
+            try
+            {
+                // Sign in with Facebook login using a server-managed flow.
+                if (IdeaManager.User == null)
+                {
+                    IdeaManager.User = await IdeaManager.DefaultManager.CurrentClient
+                        .LoginAsync(UIApplication.SharedApplication.KeyWindow.RootViewController,
+                        MobileServiceAuthenticationProvider.Facebook, "pv239-ideaapp");
+                    if (IdeaManager.User != null)
+                    {
+                        message = string.Format("You are now signed-in as {0}.", IdeaManager.User.UserId);
+                        success = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            // Display the success or failure message.
+            UIAlertView avAlert = new UIAlertView("Sign-in result", message, null, "OK", null);
+            avAlert.Show();
+
+            return success;
+        }
+
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			// Initialize Azure Mobile Apps
 			CurrentPlatform.Init();
